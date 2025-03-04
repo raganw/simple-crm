5d2f20bc0366544127d6cb0e8480d8cbb6d31335
Ragan Webber <raganw@gmail.com>
2025-03-04 10:27:25 -0800

    fix(server): properly assign user data on update
    Prior to this change, there was an issue where updating a user's
    information was not being persisted correctly.
    
    This change properly assigns the updated `age` to the user record `age`
    field, not the `id`.  Assigning `age` to `id` would result in the target
    record not being updated, and instead a new record with the incorrect
    `id` being created.
    
    Fixes #1

--- code/server/src/controllers/update-user.ts
+++ code/server/src/controllers/update-user.ts
@@ -15,7 +15,7 @@ export const updateUser = async (dataSource: DataSource, data: UpdateUserShape) 
         .findOne({ where: { id: data.id } });
     user.firstName = data.firstName;
     user.lastName = data.lastName;
-    user.id = data.age;
+    user.age = data.age;
     user.phoneNumber = data.phoneNumber;
     return dataSource.manager.getRepository(User).save(user);
 };
